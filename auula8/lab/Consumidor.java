fd45f6709deeef2f8488c0dbc8249d41a663db61
rafadias008 <123601507+rafadias008@users.noreply.github.com>
2025-10-08 01:03:16 +0000

    a

--- auula8/lab/Consumidor.java
+++ auula8/lab/Consumidor.java
@@ -1,19 +1,16 @@
-package lab;
-
-public class Produtor implements Runnable{
+public class Consumidor implements Runnable{
 
     private BufferCircular buffer;
 
-    public Produtor(BufferCircular buffer){
+    public Consumidor(BufferCircular buffer){
         this.buffer = buffer;
     }
-    
+
     @Override
     public void run(){
-        int dado = 0;
         while(true){
             ProcessUtil.atrasar(2);
-            buffer.inserir(dado);
+            buffer.remover();
         }
-    } 
+    }
 }
